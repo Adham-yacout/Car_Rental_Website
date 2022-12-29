03cfedb437ac1b15e22df8391a998df31b30fcf4
Adham-yacout <111441345+Adham-yacout@users.noreply.github.com>
2022-12-29 19:49:58 +0200

    Update rent.php

--- car rental/php/rent.php
+++ car rental/php/rent.php
@@ -1,35 +1,83 @@
 
+
+
 <?php
+ session_start();
+
 $mysql = new mysqli("localhost","root","","car_rental_system");
 mysqli_report(MYSQLI_REPORT_STRICT);
 $output ="";
+
+
+
 if(isset($_POST['Rent']))
 {	
+  
+ 
+ 
 	$plateno = $_POST['Plate_number'];
-	$reservation_id= $_POST['Reservation_number'];
+
     $startdate = $_POST['Start_date'];
 	$enddate=$_POST['End_date'];
 	$payed=$_POST['payment_amount'];
-    
+  $ssn= $_SESSION['ssn'];
+ 
+  $sql = "SELECT priceperday FROM car WHERE plate_no=?"; // SQL with parameters
+$stmt = $mysql->prepare($sql); 
+$stmt->bind_param("s", $plateno);
+$stmt->execute();
+$result = $stmt->get_result(); // get the mysqli result
+$row= $result->fetch_assoc(); // fetch the data  
 
-    
-    // $insert = $conn->prepare("INSERT INTO `payment`(`paymentid`, `amountpaid`, `amountleft`, `paymentdate`, `paymentduedate`) VALUES ()")
+ $data2= $row["priceperday"];
+ 
+
+ 
+  $timestamp1 = strtotime($startdate);
+  $timestamp2 = strtotime($enddate);
   
-    $result = $mysql -> query("INSERT INTO `reservation`(reservation_id,plate_no,paymentid,start_date,end_date,customer_ssn)  
-    values ('$reservation_id','$plateno','1','$startdate','$enddate','1')");
-    mysqli_query($mysql,$result);
+
+  $difference = ($timestamp2 - $timestamp1);
+  $difference=$difference/86400;
+  if($difference<0)
+  { 
+    
+    
+    die("start date cant come after end date");
+    header('location:../php/cars.php');
+   
+  }
+ $data=$difference*$data2;
+ 
+   
+    $result = $mysql -> query("INSERT INTO `reservation`(`plate_no`, `start_date`, `end_date`, `customer_ssn`, `amount`, `paid`) 
+    values ('$plateno','$startdate','$enddate','$ssn','$data','Y')");
+     mysqli_query($mysql,$result);
+      echo '<script>alert("Car rented succesfully")</script>';
+      header('location:../html/userland.php');
+    
     
        
 
   
 	 }
-  
-?><form action="rent.php" method = "post">
+
+?>
+<html>
+  <head>
+  <link rel="stylesheet" href="../css/stylerentcar.css">
+ 
+
+
+	
+  </head>
+  <body>
+  <form action="rent.php" method = "post">
 <div class="rent-container">
   <div class="left-container">
     <div class="puppy">
       <img src="https://i.pinimg.com/originals/4b/fa/ab/4bfaab3f829726aa86f546199645d6a5.jpg"/>
-      <link rel="stylesheet" href="../css/stylerentcar.css">
+
     </div>
   </div>
   <div class="right-container">
@@ -37,23 +85,27 @@ if(isset($_POST['Rent']))
       <h1>Rent a car!! </h1>
       <div class="set">
         <div class="Plate number">
-          <label for="Plate number">Plate number</label>
-          <input name="Plate_number" placeholder="Plate number" type="text"></input>
+          <label for="Plate number">
+           
+           plate number
+          </label>
+          <input name="Plate_number"  type="text" value="<?php echo $_GET["rent"];?>"  
+          </input>
         </div>
-        <div class="Reservation number">
+        <!-- <div class="Reservation number">
           <label for="Reservation number">Reservation number</label>
           <input name="Reservation_number" placeholder="Reservation number" type="text"></input>
-        </div>
+        </div> -->
         
       </div>
       <div class="set">
       <div class="Start date">
           <label for="Start date">Start date</label>
-          <input name="Start_date" placeholder="MM/DD/YYYY" type="date"></input>
+          <input name="Start_date" placeholder="MM/DD/YYYY" id=date1 type="date"></input>
         </div>
         <div class="End date">
           <label for="End date">End date</label>
-          <input name="End_date" placeholder="MM/DD/YYYY" type="date"></input>
+          <input name="End_date" placeholder="MM/DD/YYYY" id=date2 type="date"></input>
         </div>
       </div>
      <div class="set">
@@ -67,9 +119,12 @@ if(isset($_POST['Rent']))
     </header>
     <footer>
       <div class="set">
-        <button name="Rent">Rent</button>
+        <button name="Rent"  >Rent</button>
         </form>
       </div>
     </footer>
   </div>
 </div>
+  </body>
+</html>
+
