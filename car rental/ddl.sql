c50235232bb8bdab410c52325742e39adae0a493
Adham-yacout <111441345+Adham-yacout@users.noreply.github.com>
2022-12-29 16:48:47 +0200

    Update ddl.sql

--- car rental/ddl.sql
+++ car rental/ddl.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE car_rental_system;
 
 
-
 use car_rental_system;
 CREATE TABLE  car(
     plate_no varchar(7) PRIMARY KEY,
@@ -14,13 +13,7 @@ CREATE TABLE  car(
     priceperday double,
     image text 
 );
-CREATE TABLE payment(
-paymentid int PRIMARY KEY AUTO_INCREMENT,
-    amountpaid double,
-    amountleft double,
-    paymentdate date,
-    paymentduedate date
-);
+
 CREATE TABLE  office(
     office_id int not null PRIMARY key AUTO_INCREMENT,
     location varchar(250) not null,
@@ -28,13 +21,12 @@ CREATE TABLE  office(
 );
 
 CREATE TABLE  reservation(
-    reservation_id int not null UNIQUE,
-   plate_no varchar(7) ,
-    paymentid int,
+    reservation_id int not null AUTO_INCREMENT PRIMARY KEY,
+    plate_no varchar(7) ,
     start_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     end_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     customer_ssn int ,
-    PRIMARY KEY (customer_ssn, plate_no,start_date)
+    paid   ENUM('Y','N') NOT NULL
 );
 
 CREATE TABLE  customer(
@@ -45,7 +37,7 @@ CREATE TABLE  customer(
     country varchar(250) not null,
    `password` varchar(250) not null
 );
-ALTER TABLE reservation add FOREIGN KEY (paymentid) REFERENCES payment (paymentid);
+
 ALTER  TABLE reservation add FOREIGN key  (plate_no) REFERENCES car(plate_no);
 ALTER TABLE car add FOREIGN key  (office_id) REFERENCES office(office_id);
 ALTER TABLE reservation add FOREIGN key  (customer_ssn) REFERENCES customer(ssn);
