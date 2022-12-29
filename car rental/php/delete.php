82823662648297c342d5336fe7aa89f29d05deee
Adham-yacout <111441345+Adham-yacout@users.noreply.github.com>
2022-12-29 03:03:26 +0200

    Update delete.php

--- car rental/php/delete.php
+++ car rental/php/delete.php
@@ -1,34 +1,53 @@
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<?php require "connect.php" ; ?>
+<?php
+$output ="";
+if(isset($_POST['delete'])){
+    $id = $_POST['deletee'];
+    $sql = "DELETE FROM `car` WHERE plate_no = ?";        
+    $q = $conn->prepare($sql);
+    $response = $q->execute(array($id)); 
+    $count = $q->rowCount();
+   
+   if($count>0)
+   {
+      echo '<script>alert("Car deleted succesfully")</script>';
+   }
+ else{
+   echo '<script>alert("Car not found")</script>';
+ }
+  
+   
+   
+}
+
+?>
+<html>
+   <head>
+   <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+<link rel="stylesheet" href="../css/styledelete.css">
 
+   </head>
+
+<body>
 <section class="get-in-touch">
    <h1 class="title">Delete Car</h1>
-   <form class="contact-form row" action="../html/Adminlandpage.html">
+   <form class="contact-form row" action="delete.php" method = "post">
       <div class="form-field col-lg-6">
-         <input id="name" class="input-text js-input" type="text" required>
-         <label class="label" for="name">Plate no</label>
+         <input name="deletee" id="name" placeholder="plate_no" class="input-text js-input" type="text" required>
+         
       </div>
-      <!-- <div class="form-field col-lg-6 ">
-         <input id="email" class="input-text js-input" type="email" required>
-         <label class="label" for="email">E-mail</label>
-      </div>
-      <div class="form-field col-lg-6 ">
-         <input id="company" class="input-text js-input" type="text" required>
-         <label class="label" for="company">Company Name</label>
-      </div>
-       <div class="form-field col-lg-6 ">
-         <input id="phone" class="input-text js-input" type="text" required>
-         <label class="label" for="phone">Contact Number</label>
-      </div>
-      <div class="form-field col-lg-12">
-         <input id="message" class="input-text js-input" type="text" required>
-         <label class="label" for="message">Message</label>
-      </div> -->
-      <div class="form-field col-lg-12">
-         <input class="submit-btn" type="submit" value="delete">
-      </div>
+     
+      
+        
+         <button   class="submit-btn" name="delete" type="submit">DELETE</button>
+        
+    
    </form>
-   <link rel="stylesheet" href="../css/styledelete.css">
-</section>
+  
+</section>  
+</body>
+
+</html>
+
