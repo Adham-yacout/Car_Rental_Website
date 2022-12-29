bfe4e9a511360d79f816de76357d122a4524875a
Adham-yacout <111441345+Adham-yacout@users.noreply.github.com>
2022-12-29 22:54:55 +0200

    Update addcar.php

--- car rental/php/addcar.php
+++ car rental/php/addcar.php
@@ -58,7 +58,15 @@ try{$insert->execute([
 </head>
 
 <body>
-
+<div class="headers">
+   
+   <a href="../html/Adminlandpage.html" class ="sign" style="background: #000;
+            color: #FFF;
+            padding: 10px;
+            margin-top: 70px;
+            margin-left: 5px;
+            border-radius: 3rem;">Admin Page</a>
+ </div>
 <section class="get-in-touch">
    <h1 class="title">ADD CAR</p></h1>
    <form class="contact-form row" action="addcar.php" method="post">
